b2f4bfcd6c70dfa73ecd4683d813f400cc21bc75
cidguerrero <cid_rmc@hotmail.com>
2017-06-18 18:16:33 -0500

    wtf

--- NaiveBayesModel/NaiveBayes.py
+++ NaiveBayesModel/NaiveBayes.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[7]:
-
 get_ipython().magic('matplotlib inline')
 import seaborn
 import numpy as np
@@ -15,17 +10,29 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.model_selection import StratifiedKFold, train_test_split, KFold
 
+x = []
+y = []
 
-# Import data
-data = pd.read_csv("./zoo.csv",sep=",")
+def NaiveBayes(X_train,Y_train):
+    x = X_train
+    y = Y_train
 
+    listgnb = []
+    listmnb = []
+    listbnb = []
 
-# In[8]:
+    gnb = GaussianNB()
+    listgnb = score_seed(gnb,10,listgnb,"GNB completed")
 
-data
+    mnb = MultinomialNB()
+    listmnb = score_seed(mnb,10,listmnb,"MNB completed")
 
+    bnb = BernoulliNB()
+    listbnb = score_seed(bnb,10,listbnb,"BNB completed")
 
-# In[2]:
+    list_model = ["Gausian NB", "Multinomial NB", "Bernoulli NB"]
+    list_pref  = [np.mean(listgnb),np.mean(listmnb),np.mean(listbnb)]
+    return 
 
 def plot_correlation_map( df ):
     corr = df.corr()
@@ -40,17 +47,7 @@ def plot_correlation_map( df ):
         annot = True, 
         annot_kws = { 'fontsize' : 12 }
     )
-plot_correlation_map(data)
 
-
-# In[9]:
-
-data.drop('animal_name',axis=1,inplace=True)
-# Data and Target
-y = data['class_type']
-x = data.drop('class_type',axis = 1)
-
-# Functions
 def score(clf, train_np, random_state, folds, target):
     kf = StratifiedKFold(n_splits = folds, shuffle = False, random_state = random_state)
     list_perf = []
@@ -71,68 +68,5 @@ def score_seed(clf,nbseed,listout,printoutput):
         listout.append(list1)
     print(printoutput) 
     return listout
-
-
-# In[10]:
-
-# Modelisation
-# 10 seed differents. for each classifications we'll take 4 folds
-gnb = GaussianNB()
-listgnb = []
-listgnb = score_seed(gnb,10,listgnb,"GNB completed")
-
-mnb = MultinomialNB()
-listmnb = []
-listmnb = score_seed(mnb,10,listmnb,"MNB completed")
-
-bnb = BernoulliNB()
-listbnb = []
-listbnb = score_seed(bnb,10,listbnb,"BNB completed")
-
-print("GNB perf:",   np.mean(listgnb))
-print("MNB perf:",   np.mean(listmnb))
-print("BNB perf:",   np.mean(listbnb))
-
-
-# In[11]:
-
-list_model = ["Gausian NB", "Multinomial NB", "Bernoulli NB"]
-list_pref  = [np.mean(listgnb),np.mean(listmnb),np.mean(listbnb)]
-
-
-# In[6]:
-
-print(list_pref)
-
-
-# In[14]:
-
-# Performance of models
-fg,ax = plt.subplots(nrows=1,ncols=1,figsize=(6, 2.5))
-sns.barplot(list_model, list_pref)
-plt.ylim(0.80, 0.97)
-ax.set_ylabel("Performance")
-ax.set_xlabel("Name")
-ax.set_xticklabels(list_model,rotation=35)
-plt.title('Battle of Algorithms')
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
 
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from data.GetGeneralData import GetGeneralData
+from NaiveBayesModel.NaiveBayes import NaiveBayes
 
 
 X_train,X_test,Y_train,Y_test = GetGeneralData()
+
 
 print(X_test)
 

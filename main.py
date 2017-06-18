0b79726086e42a3fa04fa0da543443b0fe1dc9f2
Daniel Carbajal <daniel.carbajal@pucp.edu.pe>
2017-06-18 18:27:19 -0500

    neuralnetsFix

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from data.GetGeneralData import GetGeneralData
-<<<<<<< HEAD
 from NaiveBayesModel.NaiveBayes import NaiveBayes
-
-=======
 from NeuralNet.network import runNeuralNet
->>>>>>> 20eaacc2d69647bbbcdf7069a96fe673b21a2fa3
+
 
 X_train,X_test,Y_train,Y_test = GetGeneralData()
 runNeuralNet(X_train,X_test,Y_train,Y_test)
-
-
-<<<<<<< HEAD
-
-print(X_test)
-=======
->>>>>>> 20eaacc2d69647bbbcdf7069a96fe673b21a2fa3
 

cb09fa6a0293e1eedc056e5ee6c8124e0132b0a5
cidguerrero <cid_rmc@hotmail.com>
2017-06-18 20:51:05 -0500

    my first true commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from data.GetGeneralData import GetGeneralData
-from NaiveBayesModel.NaiveBayes import NaiveBayes
+from NaiveBayes.naivebayes import runNaiveBayes
 from NeuralNet.network import runNeuralNet
+
 X_train,X_test,Y_train,Y_test = GetGeneralData()
 runNeuralNet(X_train,X_test,Y_train,Y_test)
+runNaiveBayes(X_train,X_test,Y_train,Y_test)

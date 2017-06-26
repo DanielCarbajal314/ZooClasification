af0d444438702bf656903064a244c9dd68c6571f
DIA <salas.diego@pucp.pe>
2017-06-26 10:35:34 -0500

    Avance svm

--- svm/svm.py
+++ svm/svm.py
@@ -2,16 +2,29 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report,confusion_matrix
 from data.GetAnimalType import GetAnimalType
 import numpy as np
+import pandas as pd
 
 NUMBER_OF_CLASES = 7
 
-def runSVM(X_train,X_test,Y_train,Y_test):
-    #Se seleccionó svm con kernel gaussiano
-    _testSvmGaussian(X_train,X_test,Y_train,Y_test)
-    #Pruebas con otros kernels
-    #SvmKernelTest(X_train,X_test,Y_train,Y_test)
+def __getDataFromFile(fileName):
+    datafile = pd.read_csv(fileName)
+    Yvalues=datafile['class_type']
+    XColumnNames = datafile.columns.difference(['class_type','animal_name'])
+    Xvalues=datafile[XColumnNames]
+    return Xvalues,Yvalues
 
-def _testSvmGaussian(X_train,X_test,Y_train,Y_test):
+def runSVM(X_train,X_test,X_validation,Y_train,Y_test,Y_validation):
+    #Se seleccionó svm con kernel gaussiano
+    #model = _testSvmGaussian(X_train,X_validation,Y_train,Y_validation)
+    #_testSVM(model,X_test,Y_test)
+    #Pruebas con otros kernels
+    SvmKernelTest(X_train,X_validation,Y_train,Y_validation)
+
+def testModel(model,X_test,Y_test):
+    predictions = model.predict(X_test)
+    _testSVM(predictions=predictions, Y_test=Y_test)
+
+def _testSvmGaussian(X_train,X_validation,Y_train,Y_validation):
     #Se elige los mejores parámetros C y Gamma
     Cs = np.logspace(-2, 2, 9)  # ~ [0.01, 0.03, 0.1, ..., 100]
     gammas = np.logspace(-4, 4, 9)  # [0.0001, 0.001, ..., 10000]
@@ -21,24 +34,24 @@ def _testSvmGaussian(X_train,X_test,Y_train,Y_test):
     for C in Cs:
         for gamma in gammas:
 
-            model = model = SVC(kernel='rbf', C=C, gamma=gamma)
+            model = SVC(kernel='rbf', C=C, gamma=gamma)
             model.fit(X_train, Y_train)
 
-            score_val = model.score(X_test, Y_test)
+            score_val = model.score(X_validation, Y_validation)
 
             if score_val > mejor_score:
                 mejor_score = score_val
                 mejor_modelo = model
 
-    predictions = mejor_modelo.predict(X_test)
     #Se imprime un resumen de la predicción.
     print('---------------- Kernel Gaussiano----------------')
-    _testSVM(predictions=predictions,Y_test=Y_test)
+    _testSVM(mejor_modelo, X_validation, Y_validation)
     #print('Mejor valor de C: %0.4f' % mejor_modelo.get_params()['C'])
     #print('Mejor valor de gamma: %0.4f' % mejor_modelo.get_params()['gamma'])
-    return C,gamma
+    return mejor_modelo
 
-def _testSVM(predictions,Y_test):
+def _testSVM(model,X_test,Y_test):
+    predictions = model.predict(X_test)
     predictedNames = getLabeledResult(predictions)
     testNames = getLabeledResult(Y_test)
     print(confusion_matrix(testNames, predictedNames))
@@ -52,13 +65,13 @@ def getLabeledResult(predictions):
     return animalTypeNames
 
 #Kernel Test section
-def SvmKernelTest(X_train,X_test,Y_train,Y_test):
-    _SvmLinear(X_train,X_test,Y_train,Y_test)
-    _testSvmGaussian(X_train,X_test,Y_train,Y_test)
-    _SvmPolinomial(X_train,X_test,Y_train,Y_test)
-    _SvmSigmoidal(X_train,X_test,Y_train,Y_test)
+def SvmKernelTest(X_train,X_validation,Y_train,Y_validation):
+    _SvmLinear(X_train,X_validation,Y_train,Y_validation)
+    _testSvmGaussian(X_train,X_validation,Y_train,Y_validation)
+    _SvmPolinomial(X_train,X_validation,Y_train,Y_validation)
+    _SvmSigmoidal(X_train,X_validation,Y_train,Y_validation)
 
-def _SvmLinear(X_train,X_test,Y_train,Y_test):
+def _SvmLinear(X_train,X_validation,Y_train,Y_validation):
     #hiperplanos
     Cs = np.logspace(-2, 2, 9) #Se elige el parámetro C
     mejor_modelo = None
@@ -67,20 +80,19 @@ def _SvmLinear(X_train,X_test,Y_train,Y_test):
         model = model = SVC(kernel='linear', C=C)
         model.fit(X_train, Y_train)
 
-        score_val = model.score(X_test, Y_test)
+        score_val = model.score(X_validation, Y_validation)
 
         if score_val > mejor_score:
             mejor_score = score_val
             mejor_modelo = model
 
     print('---------------- Kernel Lineal -----------------')
-    predictions = mejor_modelo.predict(X_test)
     # Se imprime un resumen de la predicción.
-    _testSVM(predictions=predictions, Y_test=Y_test)
+    _testSVM(mejor_modelo, X_validation, Y_validation)
     print('Mejor valor de C: %0.4f' % mejor_modelo.get_params()['C'])
     return C
 
-def _SvmPolinomial(X_train,X_test,Y_train,Y_test):
+def _SvmPolinomial(X_train,X_validation,Y_train,Y_validation):
     #Curvas polinomiales de grado d
     Cs = np.logspace(-2, 2, 9)  # Se elige el parámetro C
     Ds = [1,2,3,4,5] #Se prueban varios grados polinomiales
@@ -91,22 +103,20 @@ def _SvmPolinomial(X_train,X_test,Y_train,Y_test):
             model = model = SVC(kernel='poly', C=1, degree = D)
             model.fit(X_train, Y_train)
 
-            score_val = model.score(X_test, Y_test)
+            score_val = model.score(X_validation, Y_validation)
 
             if score_val > mejor_score:
                 mejor_score = score_val
                 mejor_modelo = model
-
-    predictions = mejor_modelo.predict(X_test)
     # Se imprime un resumen de la predicción.
     print('---------------- Kernel Polinomial -----------------')
-    _testSVM(predictions=predictions, Y_test=Y_test)
+    _testSVM(mejor_modelo,X_validation,Y_validation)
     print('Mejor valor de C: %0.4f' % mejor_modelo.get_params()['C'])
     print('Mejor valor de grado: %d' % mejor_modelo.get_params()['degree'])
     return C, D
 
 
-def _SvmSigmoidal(X_train,X_test,Y_train,Y_test):
+def _SvmSigmoidal(X_train,X_validation,Y_train,Y_validation):
     #Se elige los mejores parámetros C y Gamma
     Cs = np.logspace(-2, 2, 9)  # ~ [0.01, 0.03, 0.1, ..., 100]
     gammas = np.logspace(-4, 4, 9)  # [0.0001, 0.001, ..., 10000]
@@ -118,16 +128,15 @@ def _SvmSigmoidal(X_train,X_test,Y_train,Y_test):
             model = model = SVC(kernel='sigmoid', C=C, gamma=gamma)
             model.fit(X_train, Y_train)
 
-            score_val = model.score(X_test, Y_test)
+            score_val = model.score(X_validation, Y_validation)
 
             if score_val > mejor_score:
                 mejor_score = score_val
                 mejor_modelo = model
 
-    predictions = mejor_modelo.predict(X_test)
     #Se imprime un resumen de la predicción.
     print('---------------- Kernel Sigmoidal ----------------')
-    _testSVM(predictions=predictions,Y_test=Y_test)
+    _testSVM(mejor_modelo, X_validation, Y_validation)
     print('Mejor valor de C: %0.4f' % mejor_modelo.get_params()['C'])
     print('Mejor valor de gamma: %0.4f' % mejor_modelo.get_params()['gamma'])
     return C,gamma
